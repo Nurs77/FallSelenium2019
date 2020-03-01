331804af33fb456d0bc6b08ea85cb2d923c546a5
Nursultan Tolebergen <nursultan.tolepbergen@gmail.com>
2020-03-01 13:54:35 -0500

    Changes ska

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // to start selenium script we need:
         // setup  webdriver(browser driver) and create webdriver
         // object
@@ -13,7 +13,18 @@ public class BasicNavigation {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         // In selenium, everything starts from WebDriver interface
-        driver.get("http://google.com"); // to open a website
+        driver.get("http://google.com");// to open a website
+        String title = driver.getTitle();
+        String exprectedTitle ="Google";
+        System.out.println("expectedTitle = " + exprectedTitle);
+        System.out.println("title = " + title);
+        if(exprectedTitle.equals(title)){
+            System.out.println("Test Passed");
+        }else{
+            System.out.println("Test Failed");
+        }
+        Thread.sleep(3000);
+        driver.close(); // to close the browser
 
 
     }
