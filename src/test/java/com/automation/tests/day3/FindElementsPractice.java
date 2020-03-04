d837252216d6b3e5c35f7d2ab77dcea4edb23eee
Nursultan Tolepbergen <maksattolepbergen@Nurs.local>
2020-03-03 20:11:18 -0500

    new change

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,4 +1,21 @@
 package com.automation.tests.day3;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class FindElementsPractice {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+
+
+
+        driver.close();
+    }
 }
